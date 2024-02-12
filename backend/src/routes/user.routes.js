8c48ad5c5656d7ac472be8c475fbaf7389dee73b
manish7479dlp <manish7479dlp@gmail.com>
2024-02-12 11:10:31 +0530

    modify: createUser input files

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,48 +1,84 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const {verifyJWT} = require("../middlewares/auth.middlewares")
-const {upload} = require("../middlewares/multer.middlewares")
+const { verifyJWT } = require("../middlewares/auth.middlewares");
+const { upload } = require("../middlewares/multer.middlewares");
 
-const {createUser, login, updateFirstName, updateLastName, updateEmail, updatePassword, updateUserDetails, deleteUser, getAllUser, addImage, deleteImage, updateAvatar, updateCoverImage} = require("../controllers/user.controllers")
+const {
+  createUser,
+  login,
+  updateFirstName,
+  updateLastName,
+  updateEmail,
+  updatePassword,
+  updateUserDetails,
+  deleteUser,
+  getAllUser,
+  addImage,
+  deleteImage,
+  updateAvatar,
+  updateCoverImage,
+} = require("../controllers/user.controllers");
 
 //create user route
-router.post("/create" , createUser);
+router.post(
+  "/create",
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    }
+  ]),
+  createUser
+);
 
 // login route
-router.post("/login" , login)
+router.post("/login", login);
 
 //update firstName route
-router.patch("/update/first-name" , verifyJWT , updateFirstName)
+router.patch("/update/first-name", verifyJWT, updateFirstName);
 
 //update lastName route
-router.patch("/update/last-name" , verifyJWT , updateLastName)
+router.patch("/update/last-name", verifyJWT, updateLastName);
 
 //update email route
-router.patch("/update/email" , verifyJWT , updateEmail)
+router.patch("/update/email", verifyJWT, updateEmail);
 
 //update email route
-router.patch("/update/password" , verifyJWT , updatePassword)
+router.patch("/update/password", verifyJWT, updatePassword);
 
 //update full user details
-router.patch("/update/user-details",verifyJWT , updateUserDetails)
+router.patch("/update/user-details", verifyJWT, updateUserDetails);
 
 //delete user details
-router.delete("/delete", verifyJWT , deleteUser)
+router.delete("/delete", verifyJWT, deleteUser);
 
-//get all user 
-router.get("/" , getAllUser)
+//get all user
+router.get("/", getAllUser);
 
-//add image 
-router.post("/upload/image",verifyJWT, upload.single("image") , addImage)
+//add image
+router.post("/upload/image", verifyJWT, upload.single("image"), addImage);
 
 //delete image
-router.delete("/image/:imgName", verifyJWT , deleteImage)
+router.delete("/image/:imgName", verifyJWT, deleteImage);
 
 //update avatar
-router.patch("/update/avatar",verifyJWT , upload.single("avatar"), updateAvatar)
+router.patch(
+  "/update/avatar",
+  verifyJWT,
+  upload.single("avatar"),
+  updateAvatar
+);
 
 //update cover image
-router.patch("/update/cover-image",verifyJWT , upload.single("coverImage"), updateCoverImage)
-
+router.patch(
+  "/update/cover-image",
+  verifyJWT,
+  upload.single("coverImage"),
+  updateCoverImage
+);
 
 module.exports = router;
