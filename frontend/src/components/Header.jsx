128e69b4fef7a07f759b01a37fa836a04fafe222
manish7479dlp <manish7479dlp@gmail.com>
2024-02-12 20:53:46 +0530

    ui: change the height of header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="bg-slate-700 w-full fixed top-0 left-0 z-50 ">
-      <nav className="container h-14 flex justify-center items-center">
+      <nav className="container h-16 flex justify-center items-center">
         <h1 className="text-white text-4xl  font-oswald">Gallery App</h1>
       </nav>
     </header>
