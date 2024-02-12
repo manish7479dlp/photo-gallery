b0ef809c1f8632d6f040d5878879b89b64eb3edf
manish7479dlp <manish7479dlp@gmail.com>
2024-02-12 23:08:59 +0530

    ui: create profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import UserProfile from "../sections/UserProfile";
+import UserGallery from "../sections/UserGallery";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="container overflow-hidden px-0 mt-16 text-white md:mt-20 flex flex-col gap-3 pb-3">
+      <UserProfile />
+      
+      <UserGallery />
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
