900fc68e67391443f3437ae557728c1f505bcdde
manish7479dlp <manish7479dlp@gmail.com>
2024-02-12 23:32:27 +0530

    feat: implement dynamic data rendering feature

--- frontend/src/sections/UserGallery.jsx
+++ frontend/src/sections/UserGallery.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ImageContainer from '../components/ImageContainer'
-const avtartImg = "https://i.pinimg.com/550x/75/06/5d/75065da93d181c15f8266289313231c6.jpg";
+import React from "react";
+import ImageContainer from "../components/ImageContainer";
+// const avtartImg = "https://i.pinimg.com/550x/75/06/5d/75065da93d181c15f8266289313231c6.jpg";
 
-const UserGallery = () => {
+const UserGallery = ({ images }) => {
+  const BASE_URL = "http://localhost:8000/";
+
   return (
     <>
-    <h1 className='text-3xl font-oswald pt-3'>Our Collections</h1>
-    <div className='border-b-2 border-yellow-600 w-40'></div>
-    <div className='py-3 bg-slate-900 flex flex-wrap gap-2 justify-center rounded-md'>
-      <ImageContainer imgUrl = {avtartImg }/>
-      
-
-    </div>
-
+      <h1 className="text-3xl font-oswald pt-3">Our Collections</h1>
+      <div className="border-b-2 border-yellow-600 w-40"></div>
+      <div className="py-3 bg-slate-900 flex flex-wrap gap-2 justify-center rounded-md">
+        {images?.map((img, idx) => {
+          const avtartImgURL = BASE_URL + img;
+          return <ImageContainer key={idx} imgUrl={avtartImgURL} />;
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default UserGallery
+export default UserGallery;
