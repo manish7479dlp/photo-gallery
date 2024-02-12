1c6078c7358f9688d2fdbbd189fb36099c4004e6
manish7479dlp <manish7479dlp@gmail.com>
2024-02-12 12:10:42 +0530

    create all pages file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile"
 const App = () => {
   return (
-    <h1 className='bg-slate-700'>hlw</h1>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/profile" element={<Profile/>} />
 
-export default App
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
