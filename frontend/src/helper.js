128319d92b6d0186bd7c438139c2b2d4bc0b475b
manish7479dlp <manish7479dlp@gmail.com>
2024-02-12 17:52:01 +0530

    feat: implement createUser functionality

---
+++ frontend/src/helper.js
@@ -0,0 +1,35 @@
+const BASE_URL = "http://localhost:8000/api/v1/user";
+
+const createUser = async (data) => {
+  try {
+    const url = BASE_URL + "/create";
+
+    const formData = new FormData();
+
+    formData.set("firstName", data.firstName);
+    formData.set("lastName", data.lastName);
+    formData.set("userName", data.userName);
+    formData.set("password", data.password);
+    formData.set("email", data.email);
+
+    if (data.avatar) {
+      formData.set("avatar", data?.avatar[0]);
+    }
+
+    if (data.coverImage) {
+      formData.set("coverImage", data.coverImage[0]);
+    }
+
+    const response = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+
+    const responseData = await response.json();
+    return responseData
+  } catch (error) {
+    console.log("Error in createUser function: ", error);
+  }
+};
+
+export { createUser };
