94f6844fc3dcd913c1f5649a04099ecd367857bc
manish7479dlp <manish7479dlp@gmail.com>
2024-02-14 00:29:29 +0530

    ui: given header bg-color in mobile view

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -38,24 +38,38 @@ const Landing = () => {
         ))}
       </div>
       <div className="absolute top-0 left-0 h-screen w-full text-white flex justify-center items-center">
-        <h1 className="absolute top-0 left-10 sm:left-24 mt-5 text-3xl md:text-5xl font-oswald">
-          <span className="text-yellow-500">Gallery </span>
-          App
-        </h1>
+        {/* header */}
+        <div className="absolute top-0 pl-10 sm:pl-20 bg-green-600 sm:bg-transparent rounded-sm w-full">
+          <h1 className=" sm:left-24 py-3 text-3xl md:text-5xl font-oswald">
+            <span className="text-yellow-500">Gallery </span>
+            App
+          </h1>
+        </div>
+
         <div className="container flex gap-3 flex-col sm:items-center">
           <h1 className="text-white text-5xl md:text-6xl font-dmsans font-extrabold">
-            Welcome to <span className="text-green-500 text-6xl"> Gallery </span> App
+            Welcome to{" "}
+            <span className="text-green-500 text-6xl"> Gallery </span> App
           </h1>
 
           <p className="text-slate-400 font-dmsans text-lg text-start tracking-wider">
-          A simple app that shows our pictures and allows us to add/remove/update them without much trouble. Some galleries have extra frills like highlight reels added in, but our gallery are straightforward...
+            A simple app that shows our pictures and allows us to
+            add/remove/update them without much trouble. Some galleries have
+            extra frills like highlight reels added in, but our gallery are
+            straightforward...
           </p>
 
           <div className="font-dmsans flex gap-8 justify-center mt-3">
-            <button className="px-4 py-1 rounded-md bg-green-500 text-xl font-bold cursor-pointer" onClick={() => navigate("/login")}>
+            <button
+              className="px-4 py-1 rounded-md bg-green-500 text-xl font-bold cursor-pointer"
+              onClick={() => navigate("/login")}
+            >
               Login
             </button>
-            <button className="px-4 py-1 rounded-md bg-yellow-500 text-xl font-bold cursor-pointer " onClick={() => navigate("/signup")}>
+            <button
+              className="px-4 py-1 rounded-md bg-yellow-500 text-xl font-bold cursor-pointer "
+              onClick={() => navigate("/signup")}
+            >
               Signup
             </button>
           </div>
