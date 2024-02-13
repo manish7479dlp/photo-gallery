cdba028b63253f7c381acad34ffead4991dcf88d
manish7479dlp <manish7479dlp@gmail.com>
2024-02-13 18:47:11 +0530

    feat: add functionality to show logout btn in only in profile page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate , useLocation } from "react-router-dom";
 const ACCESS_TOKEN = "accessToken";
 
 const Header = () => {
   const navigate = useNavigate()
+  const location = useLocation()
 
    const logout = () => {
      localStorage.removeItem(ACCESS_TOKEN)
@@ -15,7 +16,7 @@ const Header = () => {
     <header className="bg-slate-700 w-full fixed top-0 left-0 z-50 ">
       <nav className="container h-16 flex justify-between items-center">
         <h1 className="text-white text-4xl  font-oswald">Gallery App</h1>
-          <button onClick={logout} className="text-white px-3 py-1 rounded-md font-dmsans font-semibold bg-yellow-500 hover:bg-green-500">
+          <button onClick={logout} className={`text-white px-3 py-1 rounded-md font-dmsans font-semibold bg-yellow-500 hover:bg-green-500 ${location.pathname === "/profile" ? "visible" : "hidden"}`}>
             Logout
           </button>
       </nav>
