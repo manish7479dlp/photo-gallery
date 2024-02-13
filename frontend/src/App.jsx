69123c652e73a13cccfbbf47dcfb992c2d37b1ce
manish7479dlp <manish7479dlp@gmail.com>
2024-02-13 10:38:08 +0530

    add: imagePreview component route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import ImagePreview from "./components/ImagePreview";
 const App = () => {
   return (
     <BrowserRouter>
@@ -17,6 +18,8 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/image-preview/:img" element={<ImagePreview />} />
+
       </Routes>
     </BrowserRouter>
   );
