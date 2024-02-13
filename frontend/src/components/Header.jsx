6bee3629701726dfc4c2de8a29a026c27eff753a
manish7479dlp <manish7479dlp@gmail.com>
2024-02-13 23:26:54 +0530

    ui: make header hidden in landing page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
    }
 
   return (
-    <header className="bg-slate-700 w-full fixed top-0 left-0 z-50 ">
+    <header className={`${location.pathname == "/" ? "hidden" : "visible"} bg-slate-700 w-full fixed top-0 left-0 z-50 `}>
       <nav className="container h-16 flex justify-between items-center">
         <h1 className="text-white text-4xl  font-oswald">Gallery App</h1>
           <button onClick={logout} className={`text-white px-3 py-1 rounded-md font-dmsans font-semibold bg-yellow-500 hover:bg-green-500 ${location.pathname === "/profile" ? "visible" : "hidden"}`}>
