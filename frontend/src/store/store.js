337c65bc672933acce4b3ff819c1e23eb3cc54a6
manish7479dlp <manish7479dlp@gmail.com>
2024-02-14 08:45:19 +0530

    feat: add publicUser store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import userReducer from "./features/user/userSlice"
+import publicUserRoute from "./features/user/publicUser"
 const store = configureStore({
     reducer:{
         user: userReducer,
+        publicUser: publicUserRoute
     }
 })
 
