587d139f339dd75b7dc9c3535cf60de5d5cceb8d
manish7479dlp <manish7479dlp@gmail.com>
2024-02-14 20:47:35 +0530

    feat: pass only imageName

--- frontend/src/components/ImageContainer.jsx
+++ frontend/src/components/ImageContainer.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-const errorImgURL =
-  "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg";
-const ImageContainer = ({ imgUrl, img }) => {
+
+const ImageContainer = ({ imgUrl }) => {
+
   const navigate = useNavigate()
-  const imgPath = img.replace("upload\\", "")
+  const arr = imgUrl.split("/");
   return (
     <div className="">
       <img
         src={imgUrl}
         alt="img"
         className="w-40 h-40 object-cover cursor-pointer rounded-md bg-white p-[1px]"
-        onClick={() => navigate("/image-preview/"+ imgPath)}
+        onClick={() => navigate("/image-preview/"+ arr[arr.length-1])}
       />
     </div>
   );
--- frontend/src/components/ImagePreview.jsx
+++ frontend/src/components/ImagePreview.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
-const BASE_URL = "http://localhost:8000/";
 import { FaArrowCircleLeft, FaTrash, FaTimes } from "react-icons/fa";
 import { deleteImage } from "../helper";
 import { toast } from "react-toastify";
@@ -9,18 +8,18 @@ import { useDispatch } from "react-redux";
 
 const ImagePreview = () => {
   const { img } = useParams();
-  const imgUrl = BASE_URL + "upload/" + img;
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const isAuth = localStorage.getItem("user");
-
+  const imgBaseURL = "http://res.cloudinary.com/ddyo9iiz9/image/upload/v1707922147/Gallery-Images/"
+  const imgUrl = imgBaseURL + img;
 
   //delete image
   const deleteImg = async () => {
     try {
       const confirm = window.confirm("Do you really want to delete");
       if (confirm) {
-        const response = await deleteImage(img);
+        const response = await deleteImage(img.split(".")[0]);
         if (response) {
           dispatch(setData(response.data.user));
           toast.success("Image deleted sucessfully");
