32bd015e76d847561a4350bb191625831ddbb742
manish7479dlp <manish7479dlp@gmail.com>
2024-02-14 08:45:41 +0530

    create: publicUser slice

---
+++ frontend/src/store/features/user/publicUser.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+  data: {},
+
+};
+
+const publicUserSlice = createSlice({
+  name: "publicUser",
+  initialState,
+  reducers: {
+    setData: (state, action) => {
+      state.data = action.payload;
+    },
+  },
+});
+export const { setData } = publicUserSlice.actions;
+export default publicUserSlice.reducer
+
