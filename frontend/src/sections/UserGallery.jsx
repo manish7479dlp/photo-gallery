73c07dad27ca1fed4fa870f860efa45cb70edaf1
manish7479dlp <manish7479dlp@gmail.com>
2024-02-14 20:24:00 +0530

    fix: typo pass image url directly

--- frontend/src/sections/UserGallery.jsx
+++ frontend/src/sections/UserGallery.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ImageContainer from "../components/ImageContainer";
-// const avtartImg = "https://i.pinimg.com/550x/75/06/5d/75065da93d181c15f8266289313231c6.jpg";
 
 const UserGallery = ({ images }) => {
-  const BASE_URL = "http://localhost:8000/";
 
   const imagesCollection = [...images];
   imagesCollection.reverse();
@@ -17,9 +15,8 @@ const UserGallery = ({ images }) => {
       <div className="py-3 px-1 md:px-0 bg-slate-900 flex flex-wrap gap-2 justify-center rounded-md">
         {imagesCollection?.length === 0 && <h1>No Image found</h1>}
 
-        {imagesCollection?.map((img, idx) => {
-          const avtartImgURL = BASE_URL + img;
-          return <ImageContainer key={idx} imgUrl={avtartImgURL} img={img} />;
+        {imagesCollection?.map((image, idx) => {
+          return <ImageContainer key={idx} imgUrl={image} />;
         })}
       </div>
     </>
