6867e10173b9b0648fd22ace61309ce3a8f744af
manish7479dlp <manish7479dlp@gmail.com>
2024-02-15 21:40:28 +0530

    add: loading in profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,24 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import UserProfile from "../sections/UserProfile";
 import UserGallery from "../sections/UserGallery";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUserByUserName } from "../helper";
-import { toast } from "react-toastify";
 import { setData } from "../store/features/user/publicUserSlice";
+import Loading from "../components/Loading";
 
 const Profile = () => {
   let userData = useSelector((state) => state.user.data);
   const { userName } = useParams();
   const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
      if(userName) {
+      setLoading(true)
          getUserByUserName(userName).then((response) => {
              console.log(response)
              localStorage.clear()
              dispatch(setData(response.data))
+             setLoading(false)
          }).catch((error) => {
+          setLoading(false)
           console.log("Error: ",error)
          })
      }
@@ -29,7 +33,11 @@ const Profile = () => {
   if (userData.length == 0) {
     userData = useSelector((state) => state.publicUser.data);
   }
-  console.log(userData);
+
+  if(loading) {
+    return <Loading/>
+  }
+
   if (userData?.length == 0 || !userData) {
     return (
       <h1 className="pt-20 text-center text-white font-bold h-screen flex items-center justify-center text-2xl">
