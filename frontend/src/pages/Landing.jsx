793e5a68a2d00a42e1d1bb6b12743966f3fb1667
manish7479dlp <manish7479dlp@gmail.com>
2024-02-15 21:06:52 +0530

    ui: create loading component

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getAllImage } from "../helper";
 import { useNavigate } from "react-router-dom";
 import SearchField from "../components/SearchField";
+import Loading from "../components/Loading";
 
 const Landing = () => {
   const [images, setImages] = useState([]);
@@ -18,56 +19,58 @@ const Landing = () => {
   }, []);
 
   return (
-    <div className="h-screen w-full overflow-hidden">
-      <div className=" text-white grid grid-cols-3 md:grid-cols-6 md:skew-y-6  md:scale-125 opacity-15 sm:opacity-25">
-        {images.length === 0 && <h1>No image found</h1>}
+    <>
+      <div className="h-screen w-full overflow-hidden">
+        <div className=" text-white grid grid-cols-3 md:grid-cols-6 md:skew-y-6  md:scale-125 opacity-15 sm:opacity-25">
+          {images.length === 0 && <h1>No image found</h1>}
 
-        {images?.map((image, idx) => (
-          <ImageContainer imgLink={image} key={idx} />
-        ))}
-        
-      </div>
-      <div className="absolute top-0 left-0 h-screen w-full text-white flex justify-center sm:items-center">
-        {/* header */}
-        <div className="absolute top-0 pl-10 sm:pl-20 bg-green-600 sm:bg-transparent rounded-sm w-full">
-          <h1 className=" sm:left-24 py-3 text-3xl md:text-5xl font-oswald">
-            <span className="text-yellow-500">Gallery </span>
-            App
-          </h1>
+          {images?.map((image, idx) => (
+            <ImageContainer imgLink={image} key={idx} />
+          ))}
         </div>
+        <div className="absolute top-0 left-0 h-screen w-full text-white flex justify-center sm:items-center">
+          {/* header */}
+          <div className="absolute top-0 pl-10 sm:pl-20 bg-green-600 sm:bg-transparent rounded-sm w-full">
+            <h1 className=" sm:left-24 py-3 text-3xl md:text-5xl font-oswald">
+              <span className="text-yellow-500">Gallery </span>
+              App
+            </h1>
+          </div>
 
-        <div className="container flex gap-5 flex-col  mt-20 sm:mt-0">
-          <h1 className="text-white text-5xl md:text-6xl font-dmsans font-extrabold">
-            Welcome to{" "}
-            <span className="text-green-500 text-6xl"> Gallery </span> App
-          </h1>
+          <div className="container flex gap-5 flex-col  mt-20 sm:mt-0">
+            <h1 className="text-white text-5xl md:text-6xl font-dmsans font-extrabold">
+              Welcome to{" "}
+              <span className="text-green-500 text-6xl"> Gallery </span> App
+            </h1>
 
-          <p className="text-slate-400 font-dmsans text-lg text-start tracking-wider">
-            A simple app that shows our pictures and allows us to
-            add/remove/update them without much trouble. Some galleries have
-            extra frills like highlight reels added in, but our gallery are
-            straightforward...
-          </p>
+            <p className="text-slate-400 font-dmsans text-lg text-start tracking-wider">
+              A simple app that shows our pictures and allows us to
+              add/remove/update them without much trouble. Some galleries have
+              extra frills like highlight reels added in, but our gallery are
+              straightforward...
+            </p>
 
-          <SearchField/>
+            <SearchField />
 
-          <div className="font-dmsans flex gap-8 justify-center">
-            <button
-              className="px-4 py-1 rounded-md bg-green-500 text-xl font-bold cursor-pointer"
-              onClick={() => navigate("/login")}
-            >
-              Login
-            </button>
-            <button
-              className="px-4 py-1 rounded-md bg-yellow-500 text-xl font-bold cursor-pointer "
-              onClick={() => navigate("/signup")}
-            >
-              Signup
-            </button>
+            <div className="font-dmsans flex gap-8 justify-center">
+              <button
+                className="px-4 py-1 rounded-md bg-green-500 text-xl font-bold cursor-pointer"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+              <button
+                className="px-4 py-1 rounded-md bg-yellow-500 text-xl font-bold cursor-pointer "
+                onClick={() => navigate("/signup")}
+              >
+                Signup
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loading />
+    </>
   );
 };
 
