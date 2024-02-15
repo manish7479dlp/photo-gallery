47d8764e67a3b4fe341d8f3145ce4d8b1d38c28c
manish7479dlp <manish7479dlp@gmail.com>
2024-02-15 22:05:05 +0530

    fix: chagne text size in mobile view

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
   return (
     <header className={`${location.pathname == "/" ? "hidden" : "visible"}  bg-slate-700 w-full fixed top-0 left-0 z-50 `}>
       <nav className="container h-16 flex justify-between items-center">
-        <h1 className="text-white text-4xl  font-oswald">Gallery App</h1>
+        <h1 className="text-white text-2xl sm:text-4xl font-oswald">Gallery App</h1>
           <button onClick={logout} className={`text-white px-3 py-1 rounded-md font-dmsans font-semibold bg-yellow-500 hover:bg-green-500 ${location.pathname === "/profile" ? "visible" : "hidden"} ${!isAuth && "hidden"}`}>
             Logout
           </button>
