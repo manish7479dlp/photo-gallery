d628d9fab58857752366e7fe6845eb16f91ea537
manish7479dlp <manish7479dlp@gmail.com>
2024-02-15 22:12:08 +0530

    ui: chagne color of the app name

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,38 @@
 import React from "react";
-import { useNavigate , useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 const ACCESS_TOKEN = "accessToken";
 
 const Header = () => {
-  const navigate = useNavigate()
-  const location = useLocation()
+  const navigate = useNavigate();
+  const location = useLocation();
   const isAuth = localStorage.getItem("user");
 
-
-   const logout = () => {
-     localStorage.removeItem(ACCESS_TOKEN)
-     localStorage.removeItem("user")
-     navigate("/login")
-   }
+  const logout = () => {
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
 
   return (
-    <header className={`${location.pathname == "/" ? "hidden" : "visible"}  bg-slate-700 w-full fixed top-0 left-0 z-50 `}>
+    <header
+      className={`${
+        location.pathname == "/" ? "hidden" : "visible"
+      }  bg-slate-700 w-full fixed top-0 left-0 z-50 `}
+    >
       <nav className="container h-16 flex justify-between items-center">
-        <h1 className="text-white text-2xl sm:text-4xl font-oswald">Gallery App</h1>
-          <button onClick={logout} className={`text-white px-3 py-1 rounded-md font-dmsans font-semibold bg-yellow-500 hover:bg-green-500 ${location.pathname === "/profile" ? "visible" : "hidden"} ${!isAuth && "hidden"}`}>
-            Logout
-          </button>
+        <h1 className="text-white text-2xl sm:text-4xl font-oswald">
+          {" "}
+          <span className="text-green-500">Gallery </span>
+          App
+        </h1>
+        <button
+          onClick={logout}
+          className={`text-white px-3 py-1 rounded-md font-dmsans font-semibold bg-yellow-500 hover:bg-green-500 ${
+            location.pathname === "/profile" ? "visible" : "hidden"
+          } ${!isAuth && "hidden"}`}
+        >
+          Logout
+        </button>
       </nav>
     </header>
   );
